4f5e38bc45127dafedf4b0a643858cda19f8fbc0
Pavel Gnusin <p.gnusin@tinkoff.ru>
2021-06-16 17:22:52 +0300

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Spring Multi Repository Bug
 
-A sample project that demonstrates a multiple repository bug. After fix that issue, tests should be passed.
+A sample project that demonstrates a multiple repository bug. After
+fix [that issue](https://github.com/spring-projects/spring-data-commons/issues/2383), tests should be passed.
--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.10")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.data:spring-data-commons:2.6.0-SNAPSHOT")
+//    implementation("org.springframework.data:spring-data-commons:2.6.0-SNAPSHOT")
 
     runtimeOnly("com.h2database:h2")
 
